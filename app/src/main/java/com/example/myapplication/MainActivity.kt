904ba89df664e6604a766a2d252cf91822cbf857
ivan <ivan.weng@starclinic.com.tw>
2019-12-18 15:13:12 +0800

    返回鍵設定

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,8 +10,11 @@ import android.webkit.WebChromeClient
 import androidx.appcompat.app.AlertDialog
 import android.widget.Toast
 import android.util.Log
+import android.view.KeyEvent
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    private var mExitTime = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         webSettings.setDisplayZoomControls(false)
         webSettings.setSupportZoom(true)
         webSettings.setDefaultTextEncodingName("utf-8")
+        webView.setOnKeyListener(onKeyEvent)
 
         // 避免打開系統瀏覽器
         webView.setWebViewClient(object : WebViewClient() {
@@ -67,5 +71,28 @@ class MainActivity : AppCompatActivity() {
         } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
             Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    // 設定返回退出
+    override fun onBackPressed() {
+        if ((System.currentTimeMillis() - mExitTime) > 2000) {
+            Toast.makeText(this, "再點選一次「返回」退出應用", Toast.LENGTH_SHORT).show()
+            mExitTime = System.currentTimeMillis()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    // 設定返回上一頁
+    private val onKeyEvent = View.OnKeyListener { v, keyCode, event ->
+        val action = event.action
+        val webView = v as WebView
+        if (KeyEvent.ACTION_DOWN == action && KeyEvent.KEYCODE_BACK == keyCode) {
+            if (webView.canGoBack()) {
+                webView.goBack()
+                return@OnKeyListener true
+            }
+        }
+        false
     }
 }
