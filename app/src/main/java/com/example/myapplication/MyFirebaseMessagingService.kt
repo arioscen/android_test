2d9d2adcf7c63dafbe7760cfb87a246245ff84e2
ivan <ivan.weng@starclinic.com.tw>
2019-12-24 16:18:20 +0800

    handdle new token

--- app/src/main/java/com/example/myapplication/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/myapplication/MyFirebaseMessagingService.kt
@@ -3,6 +3,10 @@ package com.example.myapplication
 import com.google.firebase.messaging.RemoteMessage
 import com.google.firebase.messaging.FirebaseMessagingService
 import android.util.Log
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
@@ -36,5 +40,19 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+//        sendRegistrationToServer(token)
     }
 }
